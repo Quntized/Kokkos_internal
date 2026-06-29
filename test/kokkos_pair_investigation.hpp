7a9155ca67c68b5dddf90803a5266937113b277a
Quntized <mdsajid-09-2021911491@ne.du.ac.bd>
2026-06-29 22:13:49 +0600

    Added kokkos_pair and stuff test
    Signed-off-by: Quntized <mdsajid-09-2021911491@ne.du.ac.bd>

--- test/kokkos_pair_investigation.hpp
+++ test/kokkos_pair_investigation.hpp
@@ -1,6 +1,8 @@
 #include <Kokkos_Core.hpp>
 #include <gtest/gtest.h>
 #include <utility>
+#include <Kokkos_Pair.hpp>
+#include <mdspan/mdspan.hpp>
 
 namespace Test {
     TEST(kokkos_pair, host_execution){
@@ -26,6 +28,10 @@ namespace Test {
         std::pair<int, double> st_f_k = c_param.to_std_pair();
         EXPECT_EQ(st_f_k.first, 1);
         EXPECT_DOUBLE_EQ(st_f_k.second, 2.0);
+
+        Kokkos::pair deduction(c_param);
+        EXPECT_EQ(deduction.first, 1);
+        EXPECT_DOUBLE_EQ(deduction.second, 2.0);
     }
     void test_kokkos_pair_device(){
         Kokkos::View<Kokkos::pair<int,double>*> d_result("in device",1);
@@ -44,5 +50,48 @@ namespace Test {
     TEST(kokkos_pair,device_execution){
         test_kokkos_pair_device();
     }
+    TEST(first_of_kokkos_detail, host_execution){
+        Kokkos::pair<int, double> p(11,12.0);
+        auto first_pair = Kokkos::detail::first_of(p);
+        EXPECT_EQ(first_pair,11);
+    }
+    TEST(second_of_kokkos_detail, host_execution){
+        Kokkos::pair<int, double> p(11,12.0);
+        struct Dummy {};
+        Dummy dummy;
+        std::integral_constant<size_t, 0> dummy_int_con;
+        auto second = Kokkos::detail::last_of(dummy_int_con, dummy, p);
+        EXPECT_DOUBLE_EQ(second , 12.0);
+    }
+    #ifdef KOKKOS_ENABLE_CUDA
+    static void test_device_pair_detail_test(){
+        Kokkos::pair<int , double> p(1,2.0);
+        Kokkos::View<double*> v("initiate", 2);
+        Kokkos::parallel_for("test_detail_Kokkos_pair_in_view_in_mdspan_folder",1,KOKKOS_LAMBDA(const int i){
+            auto first = Kokkos::detail::first_of(p);
+            struct DUMMY {};
+            DUMMY dummy;
+            std::integral_constant<size_t, 0> dummy_int_const;
+            auto second = Kokkos::detail::last_of(dummy_int_const,dummy,p);
+            v(0) = first;
+            v(1) = second;
+        });
+        Kokkos::fence();
+        auto host_view = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(),v);
+        EXPECT_EQ(host_view(0),1);
+        EXPECT_EQ(host_view(1),2.0);
+    }
+    TEST(first_last_of_kokkos_detail,device_execution){
+        test_device_pair_detail_test();
+    }
+
+    #endif
+    TEST(kokkos_index_pair_like, host_execution){
+        auto val = Kokkos::detail::index_pair_like<Kokkos::pair<int,double>,int>::value;
+        EXPECT_EQ(val,true);
+    }
+
+
+
     
 }
