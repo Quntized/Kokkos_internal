271eeee7656240a9845a3393df0e2c88e803f146
Quntized <mdsajid-09-2021911491@ne.du.ac.bd>
2026-07-22 22:09:43 +0600

    kokkos view structure hierarchical
    Signed-off-by: Quntized <mdsajid-09-2021911491@ne.du.ac.bd>

--- kokkos_tree/test_dyn_rank.cpp
+++ kokkos_tree/test_dyn_rank.cpp
@@ -2,6 +2,7 @@
 #include "kokkos_core/kokkos_pair_investigation.hpp"
 #include "mdspan/kokkos_raw_mdspan.hpp"
 #include "kokkos_core/test_drank.hpp"
+#include "kokkos_core/View_data_analysis_kokkos_investigation.hpp"
 #include <Kokkos_Core.hpp>
 #include <gtest/gtest.h>
 int main(int argc, char **argv) {
