8991fe76997f2d6f26b26c84fb969d591cc9396c
Quntized <mdsajid-09-2021911491@ne.du.ac.bd>
2026-07-24 00:00:32 +0600

    mdspan some investigation on extent , maybe_static_extent and stuff
    Signed-off-by: Quntized <mdsajid-09-2021911491@ne.du.ac.bd>

--- kokkos_tree/test_dyn_rank.cpp
+++ kokkos_tree/test_dyn_rank.cpp
@@ -3,6 +3,7 @@
 #include "mdspan/kokkos_raw_mdspan.hpp"
 #include "kokkos_core/test_drank.hpp"
 #include "kokkos_core/View_data_analysis_kokkos_investigation.hpp"
+#include "mdspan/kokkos_extent_inv.hpp"
 #include <Kokkos_Core.hpp>
 #include <gtest/gtest.h>
 int main(int argc, char **argv) {
